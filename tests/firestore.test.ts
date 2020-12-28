40c75beb3c9a4ca6a274f7e0dedc33a65c88949c
hiima <hiima2@protonmail.com>
2020-12-28 20:22:43 +0900

    認証のテストを実装

--- tests/firestore.test.ts
+++ tests/firestore.test.ts
@@ -14,15 +14,6 @@ const createAuthApp = (auth?: object): firebase.firestore.Firestore => {
     .firestore();
 };
 
-// 認証なしのFirestore Appを得る
-const createUnAuthApp = (): firebase.firestore.Firestore => {
-  return firebase
-    .initializeTestApp({
-      projectId: PROJECT_ID
-    })
-    .firestore();
-};
-
 // 管理者権限で操作可能なFirestore Appを得る
 const createAdminApp = (): firebase.firestore.Firestore => {
   return firebase
@@ -32,11 +23,13 @@ const createAdminApp = (): firebase.firestore.Firestore => {
     .firestore();
 }
 
-// user情報への参照を作る
-const userRef = (db: firebase.firestore.Firestore) => db.collection('user');
+const correctUserData = {
+  name: 'suzuki taro',
+  gender: 'male',
+  age: 30
+};
 
-describe('Firestoreセキュリティルール', () => {
-  // ルールファイルの読み込み
+describe('ユーザ認証情報の検証', () => {
   beforeAll(async () => {
     await firebase.loadFirestoreRules({
       projectId: PROJECT_ID,
@@ -56,14 +49,61 @@ describe('Firestoreセキュリティルール', () => {
     await Promise.all(firebase.apps().map(app => app.delete()));
   });
 
-  test('認証がなくても読み書きが可能であること', async () => {
-    const db = createUnAuthApp();
-    const user = userRef(db).doc('test');
-    // 書き
-    await firebase.assertSucceeds(user.set({
-      name: '太郎'
-    }));
-    // 読み
-    await firebase.assertSucceeds(user.get());
+  test('自分のuidと等しいIDを持つユーザ情報だけをCRUD可能であること', async () => {
+    // taroで認証を持つDBを作成
+    const db = createAuthApp({
+      uid: 'taro'
+    });
+
+    // taroでusersコレクションへの参照を取得
+    const userDocumentRef = db.collection('users').doc('taro');
+    
+    // 自分のuidと等しいIDを持つユーザ情報を追加可能であること
+    await firebase.assertSucceeds(userDocumentRef.set(correctUserData));
+
+    // 自分のuidと等しいIDを持つユーザ情報を閲覧可能であること
+    await firebase.assertSucceeds(userDocumentRef.get());
+
+    // 自分のuidと等しいIDを持つユーザ情報を編集可能であること
+    await firebase.assertSucceeds(
+      userDocumentRef.update({
+        name: 'suzuki "DA MADAFAKA" taro'
+      })
+    );
+
+    // 自分のuidと等しいIDを持つユーザ情報を削除可能であること
+    await firebase.assertSucceeds(userDocumentRef.delete());
+  });
+
+  test('自分のuidと異なるIDを持つユーザ情報はCRUD不可能であること', async () => {
+    // 事前にadmin権限で別ユーザデータを準備
+    createAdminApp()
+      .collection('users')
+      .doc('taro')
+      .set(correctUserData);
+
+    // hanakoで認証を持つDBを作成
+    const db = createAuthApp({
+      uid: 'hanako'
+    });
+
+    // taroでusersコレクションへの参照を取得
+    const userDocumentRef = db.collection('users').doc('taro');
+
+    // 自分のuidと異なるIDを持つユーザ情報は追加不可能であること
+    await firebase.assertFails(userDocumentRef.set(correctUserData));
+
+    // 自分のuidと異なるIDを持つユーザ情報は閲覧不可能であること
+    await firebase.assertFails(userDocumentRef.get());
+
+    // 自分のuidと異なるIDを持つユーザ情報は編集不可能であること
+    await firebase.assertFails(
+      userDocumentRef.update({
+        name: 'suzuki "DA MADAFAKA" taro'
+      })
+    );
+
+    // 自分のuidと異なるIDを持つユーザ情報は削除不可能であること
+    await firebase.assertFails(userDocumentRef.delete());
   });
 });
