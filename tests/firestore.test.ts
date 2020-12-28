0b125577ccd8cf17f8a77c62b5454daa4a0da833
hiima <hiima2@protonmail.com>
2020-12-28 19:16:34 +0900

    テストファイルのテンプレートを作成

--- tests/firestore.test.ts
+++ tests/firestore.test.ts
@@ -1,5 +1,49 @@
-describe("サンプル", () => {
-  test("サンプルテスト", async () => {
-    expect(1 + 2).toBe(3);
-  })
+import * as firebase from '@firebase/testing';
+import * as fs from 'fs';
+
+const PROJECT_ID = 'qiita-demo';
+const RULES_PATH = 'firestore.rules';
+
+// 認証付きのFirestore Appを得る
+const createAuthApp = (auth?: object): firebase.firestore.Firestore => {
+  return firebase
+    .initializeTestApp({
+      projectId: PROJECT_ID,
+      auth: auth
+    })
+    .firestore();
+};
+
+// 管理者権限で操作可能なFirestore Appを得る
+const createAdminApp = (): firebase.firestore.Firestore => {
+  return firebase
+    .initializeAdminApp({
+      projectId: PROJECT_ID
+    })
+    .firestore();
+}
+
+// user情報への参照を作る
+const userRef = (db: firebase.firestore.Firestore) => db.collection('user');
+
+describe('Firestoreセキュリティルール', () => {
+  // ルールファイルの読み込み
+  beforeAll(async () => {
+    await firebase.loadFirestoreRules({
+      projectId: PROJECT_ID,
+      rules: fs.readFileSync(RULES_PATH, 'utf-8')
+    });
+  });
+
+  // Firestoreデータのクリーンアップ
+  afterEach(async () => {
+    await firebase.clearFirestoreData({
+      projectId: PROJECT_ID
+    });
+  });
+
+  // 後始末: Firestoreアプリの削除
+  afterAll(async () => {
+    await Promise.all(firebase.apps().map(app => app.delete()));
+  });
 });
